f15a84f5919eb0e92773c33ee43ce0073cefafb5
Jason Song <jsong@techempower.com>
2020-04-23 11:57:56 -0700

    Specify a fixed OS version.

--- terraform/tfb-load.tf
+++ terraform/tfb-load.tf
@@ -51,7 +51,7 @@ resource "azurerm_virtual_machine" "tfb-load" {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
-    version   = "latest"
+    version   = "16.04.202004070"
   }
   storage_os_disk {
     name              = "tfb-load-osdisk1"
